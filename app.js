e36ce371a88a5d8a8845576ad0cf9cef5abf7b4e
Santiago Argilla <sargilla@gmail.com>
2021-01-31 10:42:48 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+
+app.listen(3000, ()=>{
+    console.log('Servidor funcionando');
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.post('/',(req,res)=>{
+
+    // validar usuario y contraseña
+    
+    // si esta mal, volver a login y mostrar errores
+
+    // si esta bien , loguear y redireccionar al home
+
+    res.send(req.body);
+});
+
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.post('/register',(req,res)=>{
+    res.send(req.body);
+});
